3cb07ffdb9c5c298c4258b6508f9daeda92a0e13
iZmailvii <iameli4ckin@mail.ru>
2022-06-02 10:06:49 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-// let getRequest = (url, cb) => {
-//     let xhr = new XMLHttpRequest();
-//     // window.ActiveXObject -> xhr = new ActiveXObject()
-//     xhr.open("GET", url, true);
-//     xhr.onreadystatechange = () => {
-//         if(xhr.readyState === 4){
-//             if(xhr.status !== 200){
-//                 console.log('Error');
-//             } else {
-//                 cb(xhr.responseText);
-//             }
-//         }
-//     };
-//     xhr.send();
-// };
-
 class ProductsList {
     constructor(container = '.products'){
         this.container = container;
@@ -23,25 +7,16 @@ class ProductsList {
         this._getProducts()
             .then(data => { //data - объект js
                  this.goods = data;
-//                 console.log(data);
                  this.render()
             });
     }
-    // _fetchProducts(cb){
-    //     getRequest(`${API}/catalogData.json`, (data) => {
-    //         this.goods = JSON.parse(data);
-    //         console.log(this.goods);
-    //         cb();
-    //     })
-    // }
+
     _getProducts(){
-      
         return fetch(`${API}/catalogData.json`)
             .then(result => result.json())
             .catch(error => {
                 console.log(error);
             });
-       
     }
     calcSum(){
         return this.allProducts.reduce((accum, item) => accum += item.price, 0);
@@ -56,7 +31,6 @@ class ProductsList {
 
     }
 }
-
 
 class ProductItem {
     constructor(product, img = 'https://via.placeholder.com/200x150'){
@@ -78,5 +52,62 @@ class ProductItem {
 }
 
 let list = new ProductsList();
-console.log(list.allProducts);
+// console.log(list.allProducts);
 
+class Basket {
+    constructor(container = '.cart-block') {
+        this.container = container;
+        this.goods = [];
+
+        this._clickBasket();
+        this._getBasketItem()
+            .then(data => {
+                this.goods = data.contents;
+                this.render();
+            });
+    }
+
+    _getBasketItem() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new BasketItem();
+
+            block.insertAdjacentHTML('beforeend', productObj.render(product));
+        }
+    }
+
+    _clickBasket() {
+        document.querySelector(".btn-cart").addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('invisible');
+        })
+    }
+}
+
+class BasketItem {
+    render(product, img = 'https://via.placeholder.com/50x100') {
+        return `<div class="cart-item" data-id="${product.id_product}">
+                <div class="product-bio">
+                <img src="${img}" alt="some image">
+                <div class="product-desc">
+                <p class="product-title">${product.product_name}</p>
+                <p class="product-quantity">Quantity: ${product.quantity}</p>
+                <p class="product-single-price">${product.price} each</p>
+                </div>
+                </div>
+                <div class="right-block">
+                <p class="product-price">${product.quantity * product.price}</p>
+            <button class="del-btn" data-id="${product.id_product}">x</button>
+        </div>
+    </div>`
+    }
+}
+
+let obj = new Basket();
